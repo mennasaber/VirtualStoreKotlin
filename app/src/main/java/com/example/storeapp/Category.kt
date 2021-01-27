82485d9c8d30ac72c544ae3eeb839450d004261c
mennasaber <mennasaber0193@gmail.com>
2021-01-27 23:24:15 +0200

    Display Categories of products

---
+++ app/src/main/java/com/example/storeapp/Category.kt
@@ -0,0 +1,3 @@
+package com.example.storeapp
+
+data class Category(val id:String,val name:String,val products:List<Product>?)
---
+++ app/src/main/java/com/example/storeapp/Product.kt
@@ -0,0 +1,3 @@
+package com.example.storeapp
+
+data class Product(val id: String, val name: String, val price: Float, val categoryId: String)
