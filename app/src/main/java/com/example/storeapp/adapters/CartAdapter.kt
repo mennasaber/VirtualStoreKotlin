1427ea6379e1c5182405ff1042581361692aba74
mennasaber <mennasaber0193@gmail.com>
2021-02-19 01:37:26 +0200

    Change design

--- app/src/main/java/com/example/storeapp/adapters/CartAdapter.kt
+++ app/src/main/java/com/example/storeapp/adapters/CartAdapter.kt
@@ -29,7 +29,7 @@ class CartAdapter(private var cartList: ArrayList<CartItem>) :
 
             deleteProductIV.setOnClickListener {
                 cartList.removeAt(position)
-                notifyItemChanged(position)
+                notifyDataSetChanged()
             }
         }
     }
